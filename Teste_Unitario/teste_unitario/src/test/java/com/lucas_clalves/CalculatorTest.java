10d78d9e99135e6fb94868eb1cab3eeeb3c40aa2
lucas notbook <lucas_clalves@outlook.com>
2026-05-18 11:42:30 -0300

    testes finalizados

--- Teste_Unitario/teste_unitario/src/test/java/com/lucas_clalves/CalculatorTest.java
+++ Teste_Unitario/teste_unitario/src/test/java/com/lucas_clalves/CalculatorTest.java
@@ -1,6 +1,9 @@
 package com.lucas_clalves;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,21 +17,56 @@ public class CalculatorTest {
         calc = new Calculator();
     }
 
+    // ____________________________________________________________________________________________
     @Test
     @DisplayName("Deve retornar 0 quando nenhum argumento é passado como parametro.")
-    public void ShouldReturnZeroWhenNoValueherediven() {
+    public void shouldReturnZeroWhenNoValueherediven() {
         double result = calc.sum();
         assertEquals(0, result);
     }
 
     @Test
     @DisplayName("Deve retornar a soma entre os mumeros passados como parametro.")
-    public void ItShouldReturnTheSumOfTheNumbers(){
+    public void itShouldReturnTheSumOfTheNumbers() {
         double a = 3;
         double b = 5;
 
-        double result = calc.sum(a,b);
+        double result = calc.sum(a, b);
 
         assertEquals(8, result);
+    }
+    // ____________________________________________________________________________________________
+
+    @Test
+    @DisplayName("Deve lançar exeção caso parametro seja um numaro menor que zero")
+    public void throwAnExceptionIfTheParameterIsLessThanZero() {
+        double n = -1;
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            calc.squareRoot(n);
+        });
+
+        assertEquals("The number cannot be negative!", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Retorna a raiz quadrada do numero passado como parametro")
+    public void returnsTheSquareRootOfTheNumberPassedAsAParameter() {
+        double result = calc.squareRoot(9);
+        assertEquals(3, result);
+    }
+
+    // ____________________________________________________________________________________________
+
+    @Test
+    @DisplayName("Retorna 'true' se o numero passado for impar")
+    public void returnsTrueIfTheNumberPassedIsOdd() {
+        assertTrue(calc.isOdd(9));
+    }
+
+    @Test
+    @DisplayName("Retorna 'true' se o numero passado for impar")
+    public void returnsFalseIfTheNumberPassedIsEven() {
+        assertFalse(calc.isOdd(2));
     }
 }
